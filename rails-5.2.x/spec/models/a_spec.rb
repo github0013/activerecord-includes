f3744561939e4de4c5638d4849863915870e880a
Reedlee <write.to.lee.igor@gmail.com>
2021-12-11 18:01:11 +0100

    Prepare cases for eager load

--- rails-5.2.x/spec/models/a_spec.rb
+++ rails-5.2.x/spec/models/a_spec.rb
@@ -54,4 +54,45 @@ RSpec.describe A, type: :model do
       it { expect(A.includes(:c_type_xs, :ds).find(subject.id).ds.size).to eq 2 }
     end
   end
+
+  describe :eager_load do
+    before do
+      d = create(:d)
+      by = create(:b, a: subject, name: :y)
+      bz = create(:b, a: subject, name: :z)
+
+      c = create(:c, b: by, type_name: :x, d: d)
+      c = create(:c, b: bz, type_name: :not_x)
+      c = create(:c, b: bz, type_name: :not_x, d: d)
+      c = create(:c, b: bz, type_name: :not_x, d: create(:d))
+    end
+
+    it { expect(A.find(subject.id).bs.size).to eq 2 }
+    it { expect(A.find(subject.id).cs.size).to eq 4 }
+    it { expect(A.eager_load(:ds).find(subject.id).ds.size).to eq 2 }
+
+    describe :type_x do
+      it { expect(A.eager_load(:c_type_xs).find(subject.id).c_type_xs.size).to eq 1 }
+
+      it { expect(A.eager_load(:c_type_xs, :ds).find(subject.id).c_type_xs.size).to eq 1 }
+      it("doesn't have the problem with ordering") do
+        expect(A.eager_load(:ds, :c_type_xs).find(subject.id).c_type_xs.size).to eq 1
+      end
+
+      it { expect(A.eager_load(:ds, :c_type_xs).find(subject.id).ds.size).to eq 2 }
+      it { expect(A.eager_load(:c_type_xs, :ds).find(subject.id).ds.size).to eq 2 }
+    end
+
+    describe 'when c filtered by b' do
+      it { expect(A.eager_load(:c_of_by).find(subject.id).c_of_by.size).to eq 1 }
+
+      it { expect(A.eager_load(:c_of_by, :ds).find(subject.id).c_of_by.size).to eq 1 }
+      it("if you switch eager_load around, you get different results") do
+        expect(A.eager_load(:ds, :c_of_by).find(subject.id).c_of_by.size).to eq 1
+      end
+
+      it { expect(A.eager_load(:c_of_by, :ds).find(subject.id).ds.size).to eq 2 }
+      it { expect(A.eager_load(:ds, :c_of_by).find(subject.id).ds.size).to eq 2 }
+    end
+  end
 end
--- rails-6.1.x/app/models/a.rb
+++ rails-6.1.x/app/models/a.rb
@@ -3,4 +3,6 @@ class A < ApplicationRecord
   has_many :cs, through: :bs, source: "cs"
   has_many :c_type_xs, -> { type_x }, through: :bs, source: "cs"
   has_many :ds, -> { distinct }, through: :cs
+
+  has_many :c_of_by, -> { where(bs: {name: 'y'}) } , through: :bs, source: "cs"
 end
